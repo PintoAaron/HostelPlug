957fa63c6ed529c790d017578a9b8aed01db0245
APinto <apinto@quantumgroupgh.com>
2024-05-06 13:26:09 +0000

    error handling when publishing hostel image

--- core/utils/upload.py
+++ core/utils/upload.py
@@ -1,12 +1,20 @@
+import logging
 from firebase_admin import storage
 
 
+logger = logging.getLogger(__name__)
+
+
 def upload_image_to_storage_bucket_and_produce_url(file):
-    bucket = storage.bucket()
-    blob = bucket.blob(file.name)
+    try:
+        bucket = storage.bucket()
+        blob = bucket.blob(file.name)
 
-    blob.upload_from_file(file.file)
+        blob.upload_from_file(file.file)
 
-    blob.make_public()
-
-    return blob.public_url
+        blob.make_public()
+        logger.info(f"Success - Image uploaded to {blob.public_url}")
+        return blob.public_url
+    except Exception as e:
+        logger.error(f"Error - failed to upload image{e}")
+        return False
