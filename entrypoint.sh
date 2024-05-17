edc2360dccbb825f0d07018947dcb0f383cdb027
APinto <apinto@quantumgroupgh.com>
2024-05-17 10:28:31 +0000

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ echo "PostgreSQL is ready."
 
 # Run migrations and then start the server
 python manage.py migrate
-python manage.py collectstatic --no-input
 python manage.py create_admin
+python manage.py collectstatic --no-input
 gunicorn HostelPlug.wsgi:application --bind 0.0.0.0:8000
