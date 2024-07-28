a17874e30b31fa470ad3c21a16179ee827633166
PintoAaron <aaronpinto111@gmail.com>
2024-07-28 06:22:33 +0000

    remove compose file

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
-EXPOSE 8000
+ENTRYPOINT ["/bin/sh", "-c", "bash entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,5 @@ python manage.py migrate
 python manage.py create_admin
 python manage.py collectstatic --no-input
 gunicorn HostelPlug.wsgi:application --bind 0.0.0.0:8000
+celery -A HostelPlug worker -l info
+celery -A HostelPlug beat -l info
