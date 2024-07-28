fd51892163d1b507d27b0b75d728c1333cb9d7ec
PintoAaron <aaronpinto111@gmail.com>
2024-07-28 06:11:58 +0000

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# Wait for the database to be ready
-while ! nc -z db 5432; do
-  echo "Waiting for PostgreSQL..."
-  sleep 1
-done
-
-echo "PostgreSQL is ready."
-
-# Run migrations and then start the server
 python manage.py migrate
 python manage.py create_admin
 python manage.py collectstatic --no-input
